8c29fc9d910221e0d5cf07f992d409fa03d7e342
La Quoc Anh <hexagon.study@gmail.com>
2023-02-28 15:37:48 +0700

    fix build

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrMSIbtuCRax8vUjPx4uhZPXmNuE5sbvU",
+  authDomain: "notification-8ca55.firebaseapp.com",
+  projectId: "notification-8ca55",
+  storageBucket: "notification-8ca55.appspot.com",
+  messagingSenderId: "572071441667",
+  appId: "1:572071441667:web:ecbb515ccbe75b8627a536",
+  measurementId: "G-KXR5NK4XH8",
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
